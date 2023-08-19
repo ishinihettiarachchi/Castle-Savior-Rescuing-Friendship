7e1199634e556a9ac7fa707fecbf85124eac4127
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-19 20:00:36 +0530

    feat: modified level 1

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            CompleteLevel();
+        }
+    }
+
+    private void CompleteLevel()
+    {
+
+    }
+}
