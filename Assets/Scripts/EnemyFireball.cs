4358edc57d29c51bd89d874fa37e704fc690b329
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-08-21 17:37:43 +0530

    feat: added fireballs

---
+++ Assets/Scripts/EnemyFireball.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFireball : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force;
+    private float timer;
+    private Animator anim;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2 (direction.x, direction.y).normalized* force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x); 
+        transform.rotation = Quaternion.Euler(0,0,rot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    
+        
+    
+}
