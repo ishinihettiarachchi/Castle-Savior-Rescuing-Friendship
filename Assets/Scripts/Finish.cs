efe1472fa2d71e39d7759720a2ec5ddfe5b2b724
tharaka-19052 <tharaka-se19052@stu.kln.ac.lk>
2023-09-08 01:19:37 +0530

    feat: updated levels

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
@@ -20,6 +21,6 @@ public class Finish : MonoBehaviour
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
